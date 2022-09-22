a4d6a976ae3243a74a930f224bdb8ce98579db4e
“Kylekk812” <kang2015kyle@gmail.com>
2022-09-22 13:46:41 -0700

    finished button onClick

--- src/Product.js
+++ src/Product.js
@@ -5,15 +5,20 @@ import {React, useState} from "react";
 import './Product.css'
 
 export default function Product({item,getProduct}) {
-    // const [items, setItems] = useState(null);
+    const [items, setItems] = useState(new Array(item.length).fill(false));
+    const [currTar, setTar] = useState([1,false]);
     const [toShow, setShow] = useState(false);
     // const getId = (e) => {
     //     e.stopPropagation();
     //     console.log(e)
     // }
 
+
+    //set currTar as false to determine whether show filled in color (or not)
+    //if currTar show circle left white else show circle right blue 
+    
         return(
-         item.map((proo) => {
+         item.map((proo,ind) => {
              return (
                  <div key={proo.id} className='indiv_container'> 
                  <img src={proo.image} alt='' className="productImage" onClick={(e)=>{
@@ -25,16 +30,32 @@ export default function Product({item,getProduct}) {
                     getProduct(proo.id)
                  }}> {proo.title}</ul>
                 <br></br>
-               {!toShow? <i className="fa-solid fa-circle-chevron-left fa-2x" style= {{color:'white'}} id= {proo.id} onClick={(e)=>{
+               {/* {!toShow? <i className="fa-solid fa-circle-chevron-left fa-2x" id= {proo.id} onClick={(e)=>{
                 // changeStay(stay[proo.id-1])
                 setShow(!toShow)
+                setItems(!items[ind])
                 // getId(e)
                 getProduct(proo.id)
                }}> </i> :
-                <i className="fa-solid fa-circle-chevron-right fa-color fa-2x" onClick={(e) => {
+                <i className="fa-solid fa-circle-chevron-right fa-color fa-2x" id= {proo.id} onClick={(e) => {
                 setShow(!toShow);
                 getProduct(proo.id);
-            }}> </i> }
+            }}> </i> } */}
+         {(currTar[0] === proo.id && currTar[1])? 
+        <i className="fa-solid fa-circle-chevron-left fa-2x" id= {proo.id} onClick={
+            (e) => {
+                setShow(!toShow);
+                getProduct(proo.id);}}> </i> :
+        <i className="fa-solid fa-circle-chevron-right fa-2x" onClick={
+             (e) => {
+                 setTar([proo.id,true])
+                 setShow(!toShow);
+                 getProduct(proo.id);
+             }
+         }></i>}
+    {/* }
+} */}
+
                  </div>
              )
          })
